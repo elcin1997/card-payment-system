07b1c2b637331de58cc7594667613e061e3fadfc
elcin1997 <109592202+elcin1997@users.noreply.github.com>
2024-03-27 18:28:04 +0400

    card-costumer

---
+++ card-payment-system/cardcostumer/src/main/java/org/example/cardcostumer/CardcostumerApplication.java
@@ -0,0 +1,13 @@
+package org.example.cardcostumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcostumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardcostumerApplication.class, args);
+    }
+
+}
---
+++ card-payment-system/cardcostumer/src/test/java/org/example/cardcostumer/CardcostumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cardcostumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardcostumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
